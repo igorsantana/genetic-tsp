dcf40f7bec0efcb97e40bdc8f655f9d37e615fcd
Igor Santana <>
2016-07-25 09:21:44 -0300

    Criando a população inicial randômicamente.

--- src/estruturais/Main.java
+++ src/estruturais/Main.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.PriorityQueue;
 
 class Main {
     private FileInputStream   file;
@@ -15,9 +16,14 @@ class Main {
     private static final String TOKEN_2 = ".X.";
     private static final double PORCENTAGEM = 0.1;
     private String sequencia   = ".";
+    private static final HashMap<String, Vertice> VERTICES = new HashMap<>();
+    
+    public static Vertice getValueFromMap(String key){
+        return VERTICES.get(key);
+    }
     
     private void open() throws FileNotFoundException {
-        this.file   = new FileInputStream("/tmp/guest-Qf8Gb5/NetBeansProjects/MOA/src/estruturais/arquivo.txt");
+        this.file   = new FileInputStream("D:\\MOA\\src\\estruturais\\arquivo.txt");
         this.input  = new InputStreamReader(this.file);
         this.reader = new BufferedReader(this.input);
     }
@@ -27,44 +33,36 @@ class Main {
         
         String linha = this.reader.readLine();
         int n = Integer.parseInt(linha.trim());
-        HashMap<String, Vertice> vertices = new HashMap<>();
         
         for (int i = 0; i < n; ++i) {
-                     linha   = this.reader.readLine();
+            linha   = this.reader.readLine();
             String[] dados   = linha.split(" ");
             Vertice  vertice = new Vertice(dados[0], Double.parseDouble(dados[1]), Double.parseDouble(dados[2]));
-            vertices.put(vertice.getLabel(), vertice);
+            VERTICES.put(vertice.getLabel(), vertice);
             this.sequencia += vertice.getLabel() + TOKEN_1;
         }
         
-        System.out.println(this.sequencia);
-        
-        int p = (int) (Math.random() * n);
-        
-        System.out.println("P = " + p);
-        
-        int x = (int) (n * PORCENTAGEM);
-        
-        for (int j = 0; j < x; ++j) {
+        int tamanhoPopulacao = (int) (n * PORCENTAGEM);
+        int populacaoInicial = (int) (Math.random() * n);
+        PriorityQueue<Rota> rotas = new PriorityQueue<>((Rota o1, Rota o2) -> {
+            return (int)(o1.getDistancia() - o2.getDistancia());
+        });
+        for (int j = 0; j < tamanhoPopulacao; ++j) {
             String backup = this.sequencia;
-            for (int i = 0; i < p; ++i) {
-                int a = (int) (Math.random() * n);
-                int b = (int) (Math.random() * n);
-                while (b == a) b = (int) (Math.random() * n);
-            
-                System.out.println("A = " + a);
-                System.out.println("B = " + b);
-                System.out.println(backup);
-            
-                    backup = backup.replace(TOKEN_1 + a + TOKEN_1, TOKEN_2);
-                    backup = backup.replace(TOKEN_1 + b + TOKEN_1, TOKEN_1 + a + TOKEN_1);
-                    backup = backup.replace(TOKEN_2, TOKEN_1 + b + TOKEN_1);
-                   
-                System.out.println(backup);
+            for (int i = 0; i < populacaoInicial; ++i) {
+                int a = (int) (1 + (Math.random() * (n - 1)));
+                int b = (int) (1 + (Math.random() * (n - 1)));
+                while (b == a) b = (int) (1 + (Math.random() * (n - 1)));
+                
+                String tokenizedA = TOKEN_1 + a + TOKEN_1;
+                String tokenizedB = TOKEN_1 + b + TOKEN_1;
+                
+                backup = backup.replace(tokenizedA, TOKEN_2);
+                backup = backup.replace(tokenizedB, tokenizedA);
+                backup = backup.replace(TOKEN_2, tokenizedB);
             }   
-            System.out.println(TOKEN_1);
+            rotas.add(new Rota(backup));
         }
-        
         this.close();
     }
     
