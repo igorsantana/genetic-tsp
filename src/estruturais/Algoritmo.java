3be05ba11c1b3b465a93704a40e4c8ad2a9e3381
guest-cAwEx3 <guest-cAwEx3@DINSL012-109167>
2016-08-04 21:05:43 -0300

    Alteração na Permutação dos Filhos.

--- src/estruturais/Algoritmo.java
+++ src/estruturais/Algoritmo.java
@@ -32,12 +32,23 @@ public class Algoritmo {
         }
     }
 
-    public static void escolheReprodutores() {
+    public static void escolheReprodutores(int tamanhoTorneio, int tamanhoPopulacao) {
+        List<Rota> torneio = new ArrayList<>();
+        List<Integer> valores = new ArrayList<>();
+        //System.out.println("ROTAS = " + rotas.size());
+        while (valores.size() < tamanhoTorneio) {
+            int a = (int) (1 + (Math.random() * (tamanhoPopulacao - 1)));
+            if (!valores.contains(a)) {
+                valores.add(a);
+                torneio.add(rotas.get(a));
+            }
+        }
+        //System.out.println(torneio.size());
         Comparator<Rota> c = (o1, o2) -> (int) (o1.getDistancia() - o2.getDistancia());
-        Collections.sort(rotas, c);
+        Collections.sort(torneio, c);
         
-        reprodutores.add(rotas.get(0));
-        reprodutores.add(rotas.get(1));
+        reprodutores.add(torneio.get(0));
+        reprodutores.add(torneio.get(1));
     }
 
     public static void cruzamentoDosReprodutores() {
@@ -48,16 +59,32 @@ public class Algoritmo {
         String filhoGeradoDois = ".";
         String[] arrayPai = reprodutorPai.getChaveNormalizada().split("\\.");
         String[] arrayMae = reprodutorMae.getChaveNormalizada().split("\\.");
-        Integer k =  0;
-        Integer l =  0;
-        Integer m =  0;
-        Integer n =  0;
-        System.out.println("xxxxxxxx");
-        System.out.println(reprodutorPai.getChave());
-        System.out.println(reprodutorMae.getChave());
-        System.out.println("----");
-        for (int i = 0; i < arrayPai.length / 2; i++) {
+        
+        //System.out.println("xxxxxxxx");
+        //System.out.println(reprodutorPai.getChave());
+        //System.out.println(reprodutorMae.getChave());
+        //System.out.println("----");
+        //for (int i = 0; i < arrayPai.length / 2; i++) {
             
+            int a = (int) (1 + (Math.random() * (arrayPai.length - 1)));
+            
+            for (int x = 0; x < a; ++x) {
+                filhoGeradoUm += (arrayPai[x] + TOKEN);
+            }
+            
+            System.out.println(filhoGeradoUm);
+            /*
+            int n = 0;
+            while (a < arrayPai.length) {
+                if (!filhoGeradoUm.contains(TOKEN + arrayMae[n] + TOKEN)) {
+                    filhoGeradoUm += (arrayMae[n] + TOKEN);
+                }   
+                n ++;
+            }
+            
+            System.out.println(filhoGeradoUm);
+            
+            /*
             while (filhoGeradoUm.contains(TOKEN + arrayPai[k] + TOKEN)) k++;
             filhoGeradoUm += (arrayPai[k] + TOKEN);
             
@@ -68,14 +95,14 @@ public class Algoritmo {
             filhoGeradoDois += (arrayMae[m] + TOKEN);
             
             while (filhoGeradoDois.contains(TOKEN + arrayPai[n] + TOKEN)) n++;
-            filhoGeradoDois += arrayPai[n] + TOKEN;
-        }
+            filhoGeradoDois += arrayPai[n] + TOKEN;*/
+        //}
         reprodutores.clear();
-        System.out.println(filhoGeradoUm);
-        System.out.println(filhoGeradoDois);
-        System.out.println("xxxxxxxx");
-        filhosGerados.add(new Rota(filhoGeradoUm));
-        filhosGerados.add(new Rota(filhoGeradoDois));
+        //System.out.println(filhoGeradoUm);
+        //System.out.println(filhoGeradoDois);
+        //System.out.println("xxxxxxxx");
+        //filhosGerados.add(new Rota(filhoGeradoUm));
+        //filhosGerados.add(new Rota(filhoGeradoDois));
     }
 
     static void geraMutacao(boolean primeiroDeveMutar, boolean segundoDeveMutar) {
@@ -86,15 +113,18 @@ public class Algoritmo {
     static void atualizaPopulacao(){
         Comparator<Rota> c = (o1, o2) -> (int) (o1.getDistancia() - o2.getDistancia());
         
-        Collections.sort(rotas, c);
+        //Collections.sort(rotas, c);
 
-        filhosGerados.add(rotas.get(rotas.size() - 1));
-        filhosGerados.add(rotas.get(rotas.size() - 2));
+        //filhosGerados.add(rotas.get(rotas.size() - 1));
+        //filhosGerados.add(rotas.get(rotas.size() - 2));
         
-        Collections.sort(filhosGerados, c);
+        rotas.add(filhosGerados.get(0));
+        rotas.add(filhosGerados.get(1));
         
-        rotas.set(rotas.size() - 1, filhosGerados.get(1));
-        rotas.set(rotas.size() - 2, filhosGerados.get(0));
+        Collections.sort(rotas, c);
+        
+        rotas.remove(rotas.size() - 1);
+        rotas.remove(rotas.size() - 2);
     }
     
     static void printaPopulacao(){
